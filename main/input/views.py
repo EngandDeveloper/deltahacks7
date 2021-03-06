331330c12452bc73c096ab5c53d6d0fa3493da20
Brandon <duongb9@mcmaster.ca>
2021-03-06 17:43:07 -0500

    ready to insert input data into database

--- main/input/views.py
+++ main/input/views.py
@@ -1,20 +1,21 @@
 from django.shortcuts import render, redirect
 from .models import UserInput
 
+
 # Create your views here.
 def user_input_screen(request):
-    #user_data = UserInput.objects.get(id=id)
-    #if not request.user.is_authenticated or user_data not in request.user.author.all():
-    #    return redirect("/")
+    if not request.user.is_authenticated:
+        return redirect("/")
 
     if request.method == "POST":
         if request.POST.get("input_data"):
             blood_pressure = request.POST.get("blood_pressure")
             date_recorded = request.POST.get("date_recorded")
-
+            current_user = request.user
 
             print(blood_pressure)
             print(date_recorded)
+            print(current_user)
 
             return redirect("/")
 
