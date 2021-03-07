73e35a83a6c7bbba8087da249f000469c7e68238
Jeffrey Dang <jeffrey.dang@live.com>
2021-03-06 20:18:05 -0500

    added charting based on user input

--- main/charts/views.py
+++ main/charts/views.py
@@ -1,16 +1,29 @@
 from django.shortcuts import render
-
+from input.models import UserInput
 # Create your views here.
 
 def line_chart(request):
     dates = ['February 1','February 2','February 3','February 4']
+    sys = [120,110,125,110]
     dia = [75,80,90,70]
+    user= request.user
+    user_data = UserInput.objects.all().filter(user__username__contains=user.username)
+    dates = []
+    sys = []
+    print(type(user_data[0].current_date))
+    for data_point in user_data:
+        sys.append(str(data_point.high_blood_pressure))
+        dia.append(str(data_point.low_blood_pressure))
+        dates.append(str(data_point.current_date)[:10])
+
+    print(user_data[0].current_date)
     # queryset = Sys.objects.order_by('-date') query data from here
     # for sys in queryset:
     #     labels.append(sys.date)
     #     data.append(sys.value)
 
-    return render(request, 'line-chart.html', {
+    return render(request, 'charts/line-chart.html', {
         'labels' : dates,
-        'data': dia,
+        'dia_data': dia,
+        'sys_data': sys
     })
