85076b1a1c90d91155555d550bf278f23f38a7a2
enganddeveloper <46913754+EngandDeveloper@users.noreply.github.com>
2021-03-07 10:24:28 -0500

    Doctor Patient Dashboards

--- main/doctorpatient/views.py
+++ main/doctorpatient/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 
 from .models import Doctor, Patient
+from input.models import UserInput
 # Create your views here.
 def dashboard(request):
     isDoctor = False
@@ -23,8 +24,24 @@ def dashboard(request):
         return render(request, 'doctorpatient/doctordashboard.html', context)
     else:
         #get the last 3 measurement record here as well
+        recordList = UserInput.objects.all().filter(user__username__contains=user.username)
+        records = []
+        if len(recordList) >= 3:
+            for i in range(len(recordList)-3,len(recordList)):
+                records.append(recordList[i])
         doctorName = userPatient[0].doctor.user.username
-        context = {'doctorName':doctorName}
+
+        user_data = UserInput.objects.all().filter(user__username__contains=user.username)
+        dates = []
+        sys = []
+        dia = []
+        # print(type(user_data[0].current_date))
+        for data_point in user_data:
+            sys.append(str(data_point.high_blood_pressure))
+            dia.append(str(data_point.low_blood_pressure))
+            dates.append(str(data_point.current_date)[:10]) 
+
+        context = {'doctorName':doctorName, 'records':records, 'labels' : dates,'dia_data': dia,'sys_data': sys}
         return render(request, 'doctorpatient/patientdashboard.html', context)
 
 
@@ -52,3 +69,27 @@ def newDoctor(request):
         form = Doctor(user = user, location = location, medicalID = medicalID)
         form.save()
     return render(request, 'users/index.html')
+
+def seeUser(request):
+    if request.method == "POST":
+        user = request.POST.get('patientname')
+        userPatient = Patient.objects.all().filter(user__username__contains=user)
+        recordList = UserInput.objects.all().filter(user__username__contains=user)
+        records = []
+        if len(recordList) >= 3:
+            for i in range(len(recordList)-3,len(recordList)):
+                records.append(recordList[i])
+        doctorName = userPatient[0].doctor.user.username
+
+        user_data = UserInput.objects.all().filter(user__username__contains=user)
+        dates = []
+        sys = []
+        dia = []
+        # print(type(user_data[0].current_date))
+        for data_point in user_data:
+            sys.append(str(data_point.high_blood_pressure))
+            dia.append(str(data_point.low_blood_pressure))
+            dates.append(str(data_point.current_date)[:10]) 
+
+        context = {'doctorName':doctorName, 'records':records, 'labels' : dates,'dia_data': dia,'sys_data': sys}
+        return render(request, 'doctorpatient/patientdashboard.html', context)
